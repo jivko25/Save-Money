f26ccf6dec76cab564a1d6c6b2fb0d2773d7da4f
Zhivko Uzunov <jivkou@yahoo.com>
2025-06-24 17:29:20 +0300

    add budgets routes

--- save-money-be/index.js
+++ save-money-be/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const receiptsRouter = require('./src/controllers/receptController');
 const authRouter = require('./src/controllers/authController');
+const budgetRouter = require('./src/controllers/budgetController');
 
 const app = express();
 const PORT = 3000;
@@ -11,7 +12,8 @@ app.use(cors()); // позволява заявки от различни дом
 app.use(bodyParser.json()); // за да може да чете JSON от тялото на заявката
 
 app.use('/api/scan', receiptsRouter);
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/budget', budgetRouter);
 
 app.listen(PORT, () => {
   console.log(`Сървърът работи на http://localhost:${PORT}`);
