f6a1234800a42162d85909a28425ba2781cfab59
Zhivko Uzunov <jivkou@yahoo.com>
2025-07-07 15:51:06 +0300

    add barcode endpoint

--- save-money-be/src/controllers/cardsController.js
+++ save-money-be/src/controllers/cardsController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cardsRouter = express.Router();
 const multer = require('multer');
 const { verifySession } = require('../services/authService');
-const { createQrCardFromMultipart, getAllQrCardsForUser, getQrCardById, deleteQrCard } = require('../services/cardsService');
+const { createQrCardFromMultipart, getAllQrCardsForUser, getQrCardById, deleteQrCard, createBarcodeCard } = require('../services/cardsService');
 
 const storage = multer.memoryStorage();  // <-- тук е промяната
 const upload = multer({ storage });
@@ -10,6 +10,7 @@ const upload = multer({ storage });
 cardsRouter.use(verifySession);
 
 cardsRouter.post('/', upload.single('image'), createQrCardFromMultipart);
+cardsRouter.post('/barcode', upload.single('image'), createBarcodeCard);
 cardsRouter.get('/', getAllQrCardsForUser);
 cardsRouter.get('/:id', getQrCardById);
 cardsRouter.delete('/:id', deleteQrCard);
