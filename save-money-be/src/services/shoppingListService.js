efe30aec0fbcdc5cf3ffb1895f31652a2cb079ad
Zhivko Uzunov <jivkou@yahoo.com>
2025-07-29 12:37:48 +0300

    test modify env

--- save-money-be/src/services/shoppingListService.js
+++ save-money-be/src/services/shoppingListService.js
@@ -8,7 +8,7 @@ const client = new vision.ImageAnnotatorClient({
         type: process.env.GOOGLE_TYPE,
         project_id: process.env.GOOGLE_PROJECT_ID,
         private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Връща реален нов ред
+        private_key: process.env.GOOGLE_PRIVATE_KEY, // Връща реален нов ред
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
         client_id: process.env.GOOGLE_CLIENT_ID,
         auth_uri: process.env.GOOGLE_AUTH_URI,
@@ -206,7 +206,7 @@ async function addItemsToList(req, res) {
                 const safeExt = ['.jpg', '.jpeg', '.png', '.webp'].includes(ext.toLowerCase()) ? ext : '.jpg';
                 const uniqueFileName = `item-${uuidv4()}${safeExt}`;
                 const filePath = uniqueFileName;
-                                
+
                 console.log('Uploading image to:', filePath);
                 
                 const { error: uploadError, data: uploadData } = await supabase.storage
